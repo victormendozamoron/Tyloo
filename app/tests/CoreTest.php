500abcba3d25be2fc138d87028702084f3785d66
Julien Bonvarlet <jbonva@gmail.com>
2013-11-04 15:27:54 +0100

    Try to fix travis brah
    Signed-off-by: Julien Bonvarlet <jbonva@gmail.com>

--- app/tests/CoreTest.php
+++ app/tests/CoreTest.php
@@ -13,11 +13,31 @@ class CoreTest extends TestCase {
 	}
 
 	/**
-	 * Test Blog Page Response.
+	 * Test Blog Index Page Response.
 	 */
 	public function testGetBlogIndex()
 	{
-		$crawler = $this->client->request('GET', '/blog');
+		$crawler = $this->client->request('GET', 'blog');
+
+		$this->assertTrue($this->client->getResponse()->isOk());
+	}
+
+	/**
+	 * Test Portfolio Index Page Response.
+	 */
+	public function testGetPortfolioIndex()
+	{
+		$crawler = $this->client->request('GET', 'portfolio');
+
+		$this->assertTrue($this->client->getResponse()->isOk());
+	}
+
+	/**
+	 * Test Portfolio Index Page Response.
+	 */
+	public function testGetContactIndex()
+	{
+		$crawler = $this->client->request('GET', 'contact');
 
 		$this->assertTrue($this->client->getResponse()->isOk());
 	}
