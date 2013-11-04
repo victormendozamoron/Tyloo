5c86eaccf5c6c33a8b08c5b7fc5eb2f8539d542b
Julien Bonvarlet <jbonva@gmail.com>
2013-11-04 10:07:53 +0100

    Fixed composer.json
    Signed-off-by: Julien Bonvarlet <jbonva@gmail.com>

--- app/config/app.php
+++ app/config/app.php
@@ -116,8 +116,6 @@ return array(
 
 		// Additional Plugins
 		'Cartalyst\Sentry\SentryServiceProvider', // Sentry
-		'Way\Generators\GeneratorsServiceProvider', // Generator
-		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider', // IDE Helper
 	),
 
 	/*
